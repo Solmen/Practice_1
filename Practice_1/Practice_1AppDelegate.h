be34d3e204552d228e283487d61c95c323afbb22
Solmen <sbtolkin4@gmail.com>
2014-03-01 10:18:30 -0500

    Initial Commit

---
+++ Practice_1/Practice_1AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Practice_1AppDelegate.h
+//  Practice_1
+//
+//  Created by Samuel Tolkin on 3/1/14.
+//  Copyright (c) 2014 Samuel Tolkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Practice_1AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Practice_1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Practice_1
+//
+//  Created by Samuel Tolkin on 3/1/14.
+//  Copyright (c) 2014 Samuel Tolkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Practice_1AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Practice_1AppDelegate class]));
+    }
+}
